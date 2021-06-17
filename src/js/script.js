433a9cbbf28f9261f194263c9c1c1939fafd30ab
AsadUrRehmanCs111 <mr.asad.dev@gmail.com>
2021-06-17 12:17:20 +0500

    main screen chnaged .. Slider added.

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,4 @@
 window.addEventListener('DOMContentLoaded', event => {
-
     // Toggle the side navigation
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
     if (sidebarToggle) {
@@ -13,6 +12,37 @@ window.addEventListener('DOMContentLoaded', event => {
             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
         });
     }
+});
 
+//Slider
+var options = {
+    accessibility: true,
+    prevNextButtons: true,
+    pageDots: true,
+    setGallerySize: false,
+    arrowShape: {
+        x0: 10,
+        x1: 60,
+        y1: 50,
+        x2: 60,
+        y2: 45,
+        x3: 15
+    }
+};
 
+var $carousel = $('[data-carousel]').flickity(options);
+var $slideContent = $('.slide-content');
+var flkty = $carousel.data('flickity');
+var selectedSlide = flkty.selectedElement;
+
+flkty.on('settle', function (index) {
+    selectedSlide = flkty.selectedElement;
+});
+
+flkty.on('change', function (index) {
+    $slideContent.eq(index).removeClass('mask');
+
+    setTimeout(function () {
+        $slideContent.addClass('mask');
+    }, 500);
 });
