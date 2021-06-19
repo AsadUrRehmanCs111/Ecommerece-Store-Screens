6451dd6ad665f1b255b410da024878f04480bf1d
AsadUrRehmanCs111 <mr.asad.dev@gmail.com>
2021-06-19 11:01:03 +0500

    :tada: Screen 1 updated

--- src/js/script.js
+++ src/js/script.js
@@ -46,3 +46,20 @@ flkty.on('change', function (index) {
         $slideContent.addClass('mask');
     }, 500);
 });
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () { myFunction() };
+
+// Get the header
+var header = document.getElementById("category");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky1) {
+        header.classList.add("sticky1");
+    } else {
+        header.classList.remove("sticky1");
+    }
+}
